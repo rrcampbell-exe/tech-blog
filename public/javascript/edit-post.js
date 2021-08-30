b2efc1d64e1b2f2fd3535d90e53b90c73dac8498
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-08-30 07:00:13 -0500

    Update put method

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,17 +1,20 @@
 async function editFormHandler(event) {
   event.preventDefault();
 
+  const postData = {
+    title: document.querySelector('input[name="post-title"]').value.trim(), 
+    post_contents: document.getElementById('post-contents').value.trim(),
+  }
   const title = document.querySelector('input[name="post-title"]').value.trim();
   const post_contents = document.getElementById('post-contents').value.trim();
   const id = window.location.toString().split("/")[
     window.location.toString().split("/").length - 1
   ];
+  alert(JSON.stringify(postData))
+  alert(post_contents)
   const response = await fetch(`/api/posts/${id}`, {
     method: "PUT",
-    body: JSON.stringify({
-      title,
-      post_contents,
-    }),
+    body: JSON.stringify(postData),
     headers: {
       "Content-Type": "application/json",
     },
